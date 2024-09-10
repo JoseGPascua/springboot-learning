eeca1d9d8cfc861f885ddfb9fbd0a2a1cb9ac95d
JoseGPascua <146772240+JoseGPascua@users.noreply.github.com>
2024-09-10 14:51:23 -0700

    Added Spring Starter Validation, refer to GlobalExceptionHandler class, these exceptions will not be hit because of the custom validations and are only include for demonstration purposes

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.demo.exceptions;
 
 import com.example.demo.product.model.ErrorResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +21,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ProductNotValidException.class)
     public ResponseEntity<ErrorResponse> handleProductNotValidException(ProductNotValidException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    /**
+     * This is one way to use Spring Started Validation
+     * By having my own Custom Validations - refer to @ProductValidator and Built in Validation methods
+     * this code will not be hit.
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleProductNotValidConstraints(ConstraintViolationException exception) {
+        // Just returns first error, not all of them
+        return new ErrorResponse(exception.getConstraintViolations().iterator().next().getMessage());
     }
 }
--- src/main/java/com/example/demo/product/model/Product.java
+++ src/main/java/com/example/demo/product/model/Product.java
@@ -1,7 +1,18 @@
 package com.example.demo.product.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
+/**
+ * Note that the annotations @NotNull, @Size, and @PositiveOrZero are only used
+ * for the Spring Started Validation. These annotations provide a message for
+ * @handleProductNotValidConstraints and they will not be used because of the
+ * Custom Validations created.
+ */
 
 @Entity // This maps a Java Class to MySQL
 @Data // This comes from lombok, which allows us to not have to create Getters and Setters
@@ -13,12 +24,15 @@ public class Product {
     @Column(name = "id")
     private Integer id;
 
+    @NotNull(message = "Name is required")
     @Column(name = "name")
     private String name;
 
+    @Size(min = 20, message = "Description must be 20 characters long")
     @Column(name = "description")
     private String description;
 
+    @PositiveOrZero(message = "Price must not be negative")
     @Column(name = "price")
     private Double price;
 }
