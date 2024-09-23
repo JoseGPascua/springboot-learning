841abab64161930264837006cafa4e430042c42d
JoseGPascua <146772240+JoseGPascua@users.noreply.github.com>
2024-09-23 10:24:52 -0700

    Added caching to GetProductService and UpdateProductService, also added Custom Cache Config

--- src/main/java/com/example/demo/product/services/GetProductService.java
+++ src/main/java/com/example/demo/product/services/GetProductService.java
@@ -7,6 +7,7 @@ import com.example.demo.product.model.Product;
 import com.example.demo.product.model.ProductDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ public class GetProductService implements Query<Integer, ProductDTO> {
     }
 
     @Override
+    @Cacheable("productCache")
     public ResponseEntity<ProductDTO> execute(Integer input) {
         logger.info("Executing " + getClass() + " input: " + input);
         Optional<Product> product = productRepository.findById(input);
