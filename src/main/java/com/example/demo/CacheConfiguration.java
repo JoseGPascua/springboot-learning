b5260d2ddaa1f4f892994f120a06ea6f03d2a52c
JoseGPascua <146772240+JoseGPascua@users.noreply.github.com>
2024-10-08 16:30:22 -0700

    added feature to create new users and add them into the database

--- src/main/java/com/example/demo/CacheConfiguration.java
+++ src/main/java/com/example/demo/CacheConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 import java.util.Arrays;
 
--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -1,38 +1,26 @@
 package com.example.demo.security;
 
-import com.example.demo.mappings.Customer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User
-                .withUsername("admin")
-                .authorities("BASIC", "SPECIAL")
-                .password(encoder.encode("1")) //spring boot wont allow us to use raw test for passwords
-                .build();
-
-        UserDetails user = User
-                .withUsername("user")
-                .password(encoder.encode("2"))
-                .authorities("BASIC")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class).build();
     }
 
     @Bean
@@ -45,14 +33,16 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)  // allows for POST, PUT, DELETE mappings with authentication
                 .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/open").permitAll();
-                    authorize.requestMatchers("/closed").authenticated();
-                    authorize.requestMatchers(HttpMethod.POST, "/product").authenticated();
+                    // have to let user create new without valid credentials
+                    authorize.requestMatchers("/createnewuser").permitAll();
 
-                    authorize.requestMatchers(HttpMethod.GET, "/special").hasAuthority("SPECIAL");
-                    authorize.requestMatchers(HttpMethod.GET, "/basic").hasAnyAuthority("SPECIAL", "BASIC");
+                    //must be at the bottom
+                    authorize.anyRequest().authenticated();
                 })
-                .httpBasic(Customizer.withDefaults())   // for basic authorization
+                .addFilterBefore(
+                        new BasicAuthenticationFilter(authenticationManager(httpSecurity)),
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
     }
 }
